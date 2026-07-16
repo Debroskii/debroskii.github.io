d41c3a7a6fb1919261777d7a155869a81f058006
debroskii <debusk.elijah@gmail.com>
2026-07-16 13:40:54 -0700

    fixed map

--- japan2026/map.js
+++ japan2026/map.js
@@ -10,6 +10,14 @@ const mtLayer = L.maptiler.maptilerLayer({
     language: L.maptiler.Language.ENGLISH, //change the default language
   }).addTo(map);
 
+  L.tileLayer(`https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=${shrek}`,{ //style URL
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+    crossOrigin: true
+  }).addTo(map);
+
 // 3. Define photo data with geographic coordinates
 const photos = [
     
